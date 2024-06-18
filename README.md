a775f43d792727cae2de176f8ad643a8f4422b6b
Leo Wang <ab0988956087@gmail.com>
2024-06-18 22:37:25 +0800

    init project

---
+++ README.md
@@ -0,0 +1,42 @@
+# gemini-sh 
+
+Chat with Gemini in your terminal
+
+A simple shell script for using gemini api in your terminal. Chat like you're in the web browser.
+
+## Screenshot
+
+![Screenshot](./images/screenshot.png)
+
+
+## How to use
+
+1. Clone the repository
+```bash
+git clone https://github.com/ascodeasice/gemini-sh.git
+cd gemini-sh
+```
+
+2. Create api key file
+```bash
+echo "YOUR_API_KEY" >> gemini_api_key.txt # replace YOUR_API_KEY with your api key
+```
+
+3. Make the script executable
+```bash
+chmod +x gemini.sh
+```
+
+4. Run the script
+```bash
+./gemini.sh
+```
+
+
+## Contributing
+
+Contributions to this script are welcome! Please open an issue or a pull request on te GitHub repository.
+
+## License
+
+Tis script is licensed under the MIT License.
---
+++ gemini.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+clear
+
+SCRIPT_DIR=$(dirname "$0")
+
+API_KEY_FILE="$SCRIPT_DIR/gemini_api_key.txt"
+
+if [ ! -f "$API_KEY_FILE" ]; then
+  echo "API 金鑰文件不存在。請創建一個包含 API 金鑰的文件: $API_KEY_FILE"
+  exit 1
+fi
+
+GEMINI_API_KEY=$(cat "$API_KEY_FILE")
+
+if [ -z "$GEMINI_API_KEY" ]; then
+  echo "請先設置 GEMINI_API_KEY 環境變數。"
+  exit 1
+fi
+
+API_URL="https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}"
+
+while true; do
+  read -rp "請輸入您的問題：" question
+  # TODO: if question is empty, continue
+
+  if [ "$question" == "help" ]; then
+    echo -e "'exit' 退出\n'clear' 清除屏幕\n"
+    continue
+  fi
+
+  if [ "$question" == "exit" ]; then
+    echo "再見！"
+    break
+  fi
+
+  if [ "$question" == "clear" ]; then 
+    clear
+    continue
+  fi
+
+  # TODO: copy the last response if the question is copy
+
+  response=$(curl -s "$API_URL" -H "Content-Type: application/json" -d "{\"contents\":{\"role\":\"user\",\"parts\": [{\"text\":\"$question\"}]}}")
+
+  text=$(echo "$response" | jq -r '.candidates[0].content.parts[0].text')
+  echo -e "====================\n"
+
+  echo "$text" # TODO: handle if the response is empty
+  echo -e "\n"
+done
