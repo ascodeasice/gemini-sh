74422b6551cfaea50822bbbb9c70a523455633e1
Leo Wang <ab0988956087@gmail.com>
2024-06-19 11:27:43 +0800

    add system prompt and handle null response

--- gemini.sh
+++ gemini.sh
@@ -19,9 +19,10 @@ if [ -z "$GEMINI_API_KEY" ]; then
 fi
 
 API_URL="https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}"
+SYSTEM_PROMPT="System prompt: please respond in traditional chinese #zh-TW, unless the term is a jargon or is better for staying in English."
 
 while true; do
-  read -rp "請輸入您的問題：" question
+  read -erp "請輸入您的問題：" question
   # TODO: if question is empty, continue
 
   if [ "$question" == "help" ]; then
@@ -34,18 +35,40 @@ while true; do
     break
   fi
 
-  if [ "$question" == "clear" ]; then 
+  if [ "$question" == "clear" ]; then
     clear
     continue
   fi
 
   # TODO: copy the last response if the question is copy
 
-  response=$(curl -s "$API_URL" -H "Content-Type: application/json" -d "{\"contents\":{\"role\":\"user\",\"parts\": [{\"text\":\"$question\"}]}}")
+  response=$(curl -s "$API_URL" -H "Content-Type: application/json" \
+    -d "
+      {
+        \"contents\": [
+          {
+            role: \"user\",
+            parts: [ { \"text\": \"$SYSTEM_PROMPT\"}]
+          },
+          {
+            role: \"model\",
+            parts: [{ text: \"Understood.\"}]
+          },
+          {
+            \"role\":\"user\",
+            \"parts\": [{\"text\":\"$question\"}]
+          }
+        ]
+      }
+      ")
 
   text=$(echo "$response" | jq -r '.candidates[0].content.parts[0].text')
   echo -e "====================\n"
 
-  echo "$text" # TODO: handle if the response is empty
+  echo "$text"
+
+  if [ "$text" = "null" ]; then
+    echo "沒有收到回應。response: $response"
+  fi
   echo -e "\n"
 done
